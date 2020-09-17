e925a4cbffd9090abd8e5aa090ed6c9c477d6481
kaylazoschg <kaylazoschg@gmail.com>
2020-09-17 13:07:17 -0400

    completed studio 5

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,63 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void bracketsOutsideWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void bracketsWithinWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void bracketsBeforeWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void openBracketOnly() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void openBracketOnlyInsideWord() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code"));
+    }
+
+    @Test
+    public void closingBracketOnly() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+
+    @Test
+    public void nestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Launch]Code]"));
+    }
+
+    @Test
+    public void backwardsBracketsBeforeWord() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][LaunchCode"));
+    }
+
+    @Test
+    public void backwardsBracketsWithoutWord() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void backwardsBracketsInsideWord() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launch[Code"));
+    }
 
 }
